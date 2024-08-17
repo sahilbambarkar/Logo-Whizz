c2cfada9b906bae96d453528698eab4422b36ec6
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2024-08-18 00:10:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,24 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    port: 3334,
-    strictPort: true,
-    proxy: {
-      '/png': {
-        target: 'https://logoexpress.tubeguruji.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/png/, ''),
-      },
-    },
-  },
-  
-  },
-});
+  server:{
+    port:'3333',
+    strictPort:true,
+    proxy:{
+      '/png':'https://logoexpress.tubeguruji.com',
+    }
+  }
+
+
+
+
+})
