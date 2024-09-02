f2aa44fd835461e2683c48e308ca8bef56f38bee
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2024-09-02 20:42:09 +0530

    Update LogoPreview.jsx

--- src/components/LogoPreview.jsx
+++ src/components/LogoPreview.jsx
@@ -3,102 +3,108 @@ import { UpdateStorageContext } from './context/UpdateStorageContext';
 import { icons } from 'lucide-react';
 import html2canvas from 'html2canvas';
 
-const Base_URL = 'https://logoexpress.tubeguruji.com'; // Update with your base URL
+const Base_URL = 'https://logoexpress.tubeguruji.com';
 
 function LogoPreview({ downloadIcon }) {
-  const { updateStorage } = useContext(UpdateStorageContext);
-  const [storageValue, setStorageValue] = useState({});
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
+    const { updateStorage } = useContext(UpdateStorageContext);
+    const [storageValue, setStorageValue] = useState({});
 
-  useEffect(() => {
-    const storageData = JSON.parse(localStorage.getItem('value'));
-    setStorageValue(storageData);
-    console.log(storageData);
-  }, [updateStorage]);
+    useEffect(() => {
+        const storageData = JSON.parse(localStorage.getItem('value'));
+        setStorageValue(storageData);
+        console.log('Storage Value:', storageData);
+    }, [updateStorage]);
 
-  useEffect(() => {
-    if (downloadIcon && isImageLoaded) {
-      downloadPngLogo();
-    }
-  }, [downloadIcon, isImageLoaded]);
+    useEffect(() => {
+        if (downloadIcon) {
+            downloadPngLogo();
+        }
+    }, [downloadIcon]);
 
-  // Function to download the logo as a PNG file
-  const downloadPngLogo = () => {
-    const downloadLogoDiv = document.getElementById('downloadLogoDiv');
+    // Used for downloading the logo in PNG format
+    const downloadPngLogo = () => {
+        const downloadLogoDiv = document.getElementById('downloadLogoDiv');
 
-    html2canvas(downloadLogoDiv, {
-      backgroundColor: null,
-      useCORS: true, // Enable CORS
-    }).then(canvas => {
-      const pngImage = canvas.toDataURL('image/png');
-      const downloadLink = document.createElement('a');
-      downloadLink.href = pngImage;
-      downloadLink.download = 'SB_Logo_Whiz.png';
-      downloadLink.click();
-    }).catch(error => {
-      console.error("Error capturing logo:", error);
-    });
-  };
-
-  const getBackgroundStyle = () => {
-    if (!storageValue?.bgColor) return {};
-
-    const isGradient = storageValue.bgColor.startsWith('linear-gradient') || storageValue.bgColor.startsWith('radial-gradient');
-
-    return {
-      borderRadius: storageValue?.bgRounded,
-      background: isGradient ? storageValue.bgColor : storageValue.bgColor,
+        html2canvas(downloadLogoDiv, {
+            backgroundColor: 'rgba(255, 255, 255, 0)', // Set to transparent
+        }).then(canvas => {
+            const pngImage = canvas.toDataURL('image/png');
+            const downloadLink = document.createElement('a');
+            downloadLink.href = pngImage;
+            downloadLink.download = 'SB_Logo_Whiz.png';
+            downloadLink.click();
+        }).catch(error => {
+            console.error('Error converting to canvas:', error);
+            alert('Failed to download logo. Please try again.'); // User-friendly error message
+        });
     };
-  };
 
-  const Icon = ({ name, color, size, rotate }) => {
-    const LucideIcon = icons[name];
-    if (!LucideIcon) {
-      return null;
-    }
-    return <LucideIcon color={color} size={size} style={{ transform: `rotate(${rotate}deg)` }} />;
-  };
+    const getBackgroundStyle = () => {
+        if (!storageValue?.bgColor) return {};
 
-  const iconPath = storageValue?.icon?.includes('.png')
-    ? `${Base_URL}/png/${storageValue?.icon}`
-    : null;
+        // Check if the value is a gradient
+        const isGradient = storageValue.bgColor.startsWith('linear-gradient') || storageValue.bgColor.startsWith('radial-gradient');
 
-  return (
-    <div className='flex items-center justify-center h-screen w-full'>
-      <div className='h-[500px] w-[500px] bg-gray-200 outline-dotted outline-gray-300'
-        style={{
-          padding: storageValue?.bgPadding
-        }}>
-        <div
-          id='downloadLogoDiv'
-          className='h-full w-full flex items-center justify-center'
-          style={getBackgroundStyle()}
-        >
+        return {
+            borderRadius: storageValue?.bgRounded,
+            background: isGradient ? storageValue.bgColor : storageValue.bgColor,
+        };
+    };
 
-          {storageValue?.icon?.includes('.png') ? (
-            <img
-              src={iconPath}
-              alt="Logo"
-              onLoad={() => setIsImageLoaded(true)}
-              style={{
-                height: storageValue?.iconSize,
-                width: storageValue?.iconSize,
-                transform: `rotate(${storageValue?.iconRotate}deg)`,
-                transition: 'transform 0.3s ease',
-              }}
+    const Icon = ({ name, color, size, rotate }) => {
+        const LucideIcon = icons[name];
+        if (!LucideIcon) {
+            return null;
+        }
+        return (
+            <LucideIcon
+                color={color}
+                size={size}
+                style={{
+                    transform: `rotate(${rotate}deg)`,
+                }}
             />
-          ) : (
-            <Icon
-              name={storageValue?.icon}
-              color={storageValue?.iconColor}
-              size={storageValue?.iconSize}
-              rotate={storageValue?.iconRotate}
-            />
-          )}
+        );
+    };
+
+    return (
+        <div className="flex items-center justify-center h-screen w-full">
+            <div
+                className="h-[500px] w-[500px] bg-gray-200 outline-dotted outline-gray-300"
+                style={{
+                    padding: storageValue?.bgPadding,
+                }}
+            >
+                <div
+                    id="downloadLogoDiv"
+                    className="h-full w-full flex items-center justify-center"
+                    style={getBackgroundStyle()}
+                >
+                    {/* Check if the icon is a PNG image or a Lucide icon */}
+                    {storageValue?.icon?.includes('.png') ?
+                        // Handle PNG images
+                        <img
+                            src={`${Base_URL}/png/${storageValue?.icon}`} // Use absolute URL
+                            style={{
+                                height: storageValue?.iconSize,
+                                width: storageValue?.iconSize,
+                                transform: `rotate(${storageValue?.iconRotate}deg)`,
+                                transition: 'transform 0.3s ease',
+                            }}
+                        />
+                        :
+                        // Handle Lucide icons
+                        <Icon
+                            name={storageValue?.icon}
+                            color={storageValue?.iconColor}
+                            size={storageValue?.iconSize}
+                            rotate={storageValue?.iconRotate}
+                        />
+                    }
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default LogoPreview;
